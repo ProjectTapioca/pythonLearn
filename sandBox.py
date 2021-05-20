ecd799e53efc32c4fe3f7c7ee0ab44e21a4bd58a
Luke Ugalino <39897299+LumpiaNation@users.noreply.github.com>
2021-05-20 15:55:18 -0700

    Update 2

--- sandBox.py
+++ sandBox.py
@@ -1,18 +1,10 @@
 import math
-
-#Going to have a while loop run while it also performs
-#some math equations
-
-x = 3
-listNum = [25, 30, 35, 40, 45, 50]
-while x != 0:
-    for numS in listNum:
-        print(numS)
-        y = numS + 3
-        print(y)
-    x -= 1
-
-
+perc = 1/3
+coco_wt = 1450
+macaw_wt = 900
+macaw_limit = macaw_wt * perc
+num_macaws = coco_wt / macaw_limit
+print(math.ceil(num_macaws))
 
 
 
